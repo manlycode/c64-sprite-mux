75f2860b7b7a10d04619b54469f7e8a57bed0e18
Chris Rittersdorf <chris.rittersdorf@collectiveidea.com>
2020-12-12 15:29:05 -0500

    Even raster stripes.

--- src/main.asm
+++ src/main.asm
@@ -7,10 +7,10 @@
 #import "chipset/lib/vic2-global.asm"
 #import "chipset/lib/mos6510-global.asm"
 
-.label IRQ_1 = 150
-.label IRQ_2 = 200
-.label IRQ_3 = 220
-.label IRQ_4 = 300
+.label IRQ_1 = 22
+// .label IRQ_2 = 200
+// .label IRQ_3 = 220
+// .label IRQ_4 = 300
 
 .pc = $0801 "Basic Upstart"
 :BasicUpstart(start) // Basic start routine
@@ -28,6 +28,8 @@ start:
 
   lda #c64lib.IMR_RASTER      // VIC-II is about to produce raster interrupt
   sta c64lib.IMR
+  dec c64lib.BG_COL_0         // change background color 
+  dec c64lib.BORDER_COL       // change border color
   c64lib_setRaster(IRQ_1)
   lda #<irq1
   sta c64lib.IRQ_LO
@@ -42,32 +44,34 @@ start:
 block:
   jmp block                   // go into endless loop
   
-irq1: {  
-  c64lib_irqEnter()
-  inc c64lib.BG_COL_0         // change background color 
-  inc c64lib.BORDER_COL       // change border color
-  c64lib_irqExit(irq2, IRQ_2, false)
-}  
 
-irq2: {
-  c64lib_irqEnter()
-  dec c64lib.BG_COL_0         // change it back
-  dec c64lib.BORDER_COL       // change it back
-  c64lib_irqExit(irq3, IRQ_3, false)
-}
+.const MAX_ROW=13
+.const ROW_HEIGHT = 22
 
-irq3: {
-  c64lib_irqEnter()
-  inc c64lib.BG_COL_0         // change it back
-  inc c64lib.BORDER_COL       // change it back
-  c64lib_irqExit(irq4, IRQ_4, false)
-}
+irq1:
 
-irq4: {
-  c64lib_irqEnter()
-  dec c64lib.BG_COL_0         // change it back
-  dec c64lib.BORDER_COL       // change it back
-  c64lib_irqExit(irq1, IRQ_1, false)
+.for (var i = 1; i <= MAX_ROW; i += 2) {
+    .var row1 = IRQ_1+i*ROW_HEIGHT
+    .var row2 = IRQ_1+((i+1)*ROW_HEIGHT)
+.label odd = *    
+    c64lib_irqEnter()
+    inc c64lib.BG_COL_0         // change background color 
+    inc c64lib.BORDER_COL       // change border color
+    c64lib_irqExit(even, row1, false)
+
+.label even = *
+    .if (i==MAX_ROW) {
+        c64lib_irqEnter()
+        dec c64lib.BG_COL_0         // change background color 
+        dec c64lib.BORDER_COL       // change border color
+        c64lib_irqExit(irq1, 0, false)   
+    } else {
+        c64lib_irqEnter()
+        dec c64lib.BG_COL_0         // change background color 
+        dec c64lib.BORDER_COL       // change border color
+        c64lib_irqExit(oddNext, row2, false)
+    }
+.label oddNext = *
 }
   
 irqFreeze: {
